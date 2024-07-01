c505bf19ce38aa0558e346f97082c0dd6e565c9f
Pratyush Kumar <pratyushkumar1304@gmail.com>
2024-07-01 11:09:42 +0530

    made the elements move

--- script.js
+++ script.js
@@ -2,15 +2,16 @@ const yesButton = document.getElementById('yes')
 const noButton = document.getElementById('no')
 let noCounter = 0 //  const will lead to error as it cant be assigned
 yesButton.addEventListener("click" , (e)=>{
-    alert(`hey!!,I'm clicked`)
+    alert(`I too Love YOU!!`)
 
 })
 
 noButton.addEventListener('click', (e)=>{
   //  e.preventDefault()
    noCounter++
-  alert(noCounter)
+  // alert(noCounter)
   noButton.style.position = "absolute"
-  noButton.style.top =  Math.floor((Math.random()*100)+1)
-  noButton.style.left =  Math.floor((Math.random()*30)+1)
+  
+  noButton.style.top =  `${(Math.floor((Math.random()*100)+1))}px`
+  noButton.style.left =  `${Math.floor((Math.random()*300)+1)}px`
 })
